75793cbd241e674ec0acad002f8fd275388ad700
GharsallahDev <118175079+GharsallahDev@users.noreply.github.com>
2024-08-16 15:13:38 +0100

    Render Deploy

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,14 @@
-from app import create_app
+import os
+from flask_migrate import upgrade
+from app import create_app, db
 
 app = create_app()
+
+def run_migrations():
+    with app.app_context():
+        upgrade()
+
+if __name__ == "__main__":
+    run_migrations()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
